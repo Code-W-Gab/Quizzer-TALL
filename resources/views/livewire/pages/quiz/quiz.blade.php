60142c988b2b79ee6b715a6bcd755fbe542f2cad
Code-W-Gab <gabconcepcion02@gmail.com>
2026-08-23 23:58:48 +0800

    Update files

--- resources/views/livewire/pages/quiz/quiz.blade.php
+++ resources/views/livewire/pages/quiz/quiz.blade.php
@@ -1,12 +1,17 @@
 <x-app-layout>
     <main class="flex justify-center">
-        <div class="w-200 mt-6">
-            <div class="bg-white rounded-xl border border-gray-200 p-4 space-y-4">
+        <div class="mt-6 space-y-4">
+            <div class="bg-white rounded-xl border border-gray-200 p-4 space-y-4 w-200">
                 <div class="flex items-center justify-between gap-4">
                     <h3>Question 1 of 3</h3>
                     <div class="bg-blue-50 text-blue-500 px-2 py-0.5 rounded-xl text-sm">multiple choices</div>
                 </div>
                 <x-progress-bar :value="75" />
+            </div>
+
+            <div class="w-200 space-y-4 bg-white rounded-xl border-gray-200 p-4">
+                <h1 class="font-bold">What is the hottest planet</h1>
+
             </div>
         </div>
     </main>
